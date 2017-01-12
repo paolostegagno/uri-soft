9d73630824a0b3b5011cf62cc63b9ada4379051c
Paolo <pstegagno@uri.edu>
2017-01-12 16:17:18 -0500

    created class container for bridge map

---
+++ uri_bridge/include/uri_bridge/bridge_map.hpp
@@ -0,0 +1,29 @@
+
+
+
+#ifndef __BRIDGE_MAP_HPP__
+#define __BRIDGE_MAP_HPP__
+
+using namespace uri;
+
+namespace uri_bridge{
+	
+	class BridgeMap{
+		
+	private:
+		
+		
+		
+		
+	public:
+		
+		BridgeMap(){}
+		
+		
+	};
+	
+	
+};
+
+
+#endif
---
+++ uri_bridge/src/bridge_map.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+#include "ros/ros.h"
+#include <uri_bridge/bridge_map.hpp>
+
+namespace uri_bridge{
+
+  
+  
+  
+
+
+
+};
+
+
